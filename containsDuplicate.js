b12080320f9d8cbea3d789cf168a1762fcd554c9
Ian Ellis <ianellis015@gmail.com>
2025-01-29 12:40:35 -0800

    containsDuplicate in javascript

---
+++ containsDuplicate.js
@@ -0,0 +1,16 @@
+class Solution {
+    hasDuplicate(nums) {
+        const set  = new Set();
+        for (let i of nums) {
+            if (set.has(i)) {
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+}
+
+const solution = new Solution();
+const arr = [1,2,3]
+console.log(solution.hasDuplicate(arr))
